a086f07e7afa4dc5b060056dd54e1eb0b1b4811b
MIchael  Kmiecik <93628363+kmiecik013@users.noreply.github.com>
2022-08-25 17:36:19 -0600

    added Home page to app

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import background from "../images/background2.jpeg";
+import blurpic from "../images/blur3.jpg";
+import '../styles/home.css'
+import '../styles/home2.css'
+
+export default function Home() {
+  return (
+    <div className="home-page" id="home">
+  
+      <div className="fade-in-text">
+      
+
+        
+        <p>“I breathe in calmness, I breathe out stress”</p>  
+        </div>
+
+
+    </div>
+  );
+}
